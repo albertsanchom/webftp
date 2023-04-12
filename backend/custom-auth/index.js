d73d6fe05e6de426d457760e82e402b47528b894
David <davixyz@gmail.com>
2023-04-12 13:24:34 +0200

    test cookie auth

--- backend/custom-auth/index.js
+++ backend/custom-auth/index.js
@@ -68,7 +68,7 @@ async function getPermissions(user){
 module.exports.handler = async (event, context, callback) => {
   console.log(JSON.stringify(event));
   //const token = event.authorizationToken;
-  const token = getCookie("oidc_token", event.headers.Cookie);
+  const token = getCookieValue("oidc_token", event.headers.Cookie);
   if(!token){
     return ('Unauthorized ', 'No token'); // Return a 401 Unauthorized response
   }
