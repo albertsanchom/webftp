2fa6dfa5b4aec20720ed4dc76836bf1726bdbfb2
David Ayala Sánchez <david.ayala@uoc.edu>
2023-04-28 08:09:43 +0200

    logout

--- frontend/src/components/Controls.vue
+++ frontend/src/components/Controls.vue
@@ -1,12 +1,13 @@
 <template>
     <div id="actions" v-if="isLogged">
-        <h2 class="pull-left">Hola {{userName}} <a :href="logoutPath+`logout`">Logout</a></h2>
+        <h2 class="pull-left">Hola {{userName}}</h2>
         <span v-if="!authError">
         <a href="#" v-on:click.prevent="sendAction('create')" v-if="!isRoot && !isRootForUser && readWrite==='rw'" class="pull-right"><i class="fa fa-folder-plus fa-2x" aria-hidden="true" :title="$t('actionAdd')"></i></a>
         <a href="#" v-on:click.prevent="sendAction('browse')" v-if="!isRoot && !isRootForUser" class="pull-right"><i class="fa fa-sync fa-2x" aria-hidden="true" :title="$t('actionRefresh')"></i></a>
         <a href="#" v-on:click.prevent="sendAction('download')" v-if="!isRoot && !isRootForUser" class="pull-right"><i class="fa fa-download fa-2x" aria-hidden="true" :title="$t('actionDownload')"></i></a>
         <a href="#" v-on:click.prevent="sendAction('delete')" v-if="!isRoot && !isRootForUser && readWrite==='rw'" class="pull-right icon-delete"><i class="fa fa-trash fa-2x" aria-hidden="true" :title="$t('actionDelete')"></i></a>
         </span>
+        <a :href="endpoint+`logout`"><i class="fa fa-right-from-bracket fa-2x" aria-hidden="true" :title="$t('actionLogout')"></i></a>
     </div>
 </template>
 
@@ -58,9 +59,6 @@
   export default {
     name : 'ControlsComponent',
     props : ['isRoot','isRootForUser', 'userName', 'isLogged', 'authError', 'readWrite', 'endpoint'],
-    data(){
-      this.logoutPath = this.endpoint + (window.localStorage.getItem("login")||"");
-    },
     methods: {
         sendAction(action, data){
           this.$emit('action', action, data);
